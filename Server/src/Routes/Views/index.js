9621cfe7a06e9a23528922e19e242107388faa90
Edgar Figueroa <edgar.figueroa.gtz@gmail.com>
2020-07-02 22:08:07 -0500

    Debug node app

--- Server/src/Routes/Views/index.js
+++ Server/src/Routes/Views/index.js
@@ -1,6 +1,8 @@
 const route = require('express').Router()
 
-route.get('/', (req, res) => res.send('Hola esta es la vista inicial'))
+route.get('/', (req, res) => {
+  res.send('Hola esta es la vista inicial')
+})
 
 
 module.exports = route
